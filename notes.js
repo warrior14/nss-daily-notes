15a3c5c958855c3f9eac1686c9a985e6b713be71
warrior14 <ironaddicts14@gmail.com>
2021-05-27 11:09:10 -0500

    my first version of daily-notes

---
+++ notes.js
@@ -0,0 +1,19 @@
+const notes = [
+    {
+        id: 1,
+        subject: "git terminal",
+        date: "May 20, 2021",
+        feeling: "It felt very hard to comprehend and memorized. But through repetitions I feel a bit more comfortable with it.",
+        timeSpent: 1800
+    },
+    {
+        id: 2,
+        subject: "html and css",
+        date: "May 20, 2021",
+        feeling: "It seemed relatively easy and very fun to dive into! Unlike other subjects ...",
+        timeSpent: 3000
+    }
+];
+
+
+console.log(notes);
