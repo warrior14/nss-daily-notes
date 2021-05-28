f551a7fd2de3edc0df2bd37a4c68823d3afead9a
warrior14 <ironaddicts14@gmail.com>
2021-05-28 15:25:59 -0500

    Adding a note function and automating the id and also added a time and date property

--- notes.js
+++ notes.js
@@ -46,10 +46,43 @@ notes.push(noteAboutToday);
 
 //  Finding a note exercise using a for of loop with if statement
 
-const searchTerm = 10
+// const searchTerm = 10
 
-for (const note of notes) {
-    if (note.timeSpent === searchTerm) {
-        console.log(note);
-    };
+// for (const note of notes) {
+//     if (note.timeSpent === searchTerm) {
+//         console.log(note);
+//     };
+// };
+
+
+// Writing New Note with no Id exercise 
+
+const createNote = (note) => {
+    const lastIndex = notes.length - 1
+    const currentLastNote = notes[lastIndex]
+    const maxId = currentLastNote.id
+    const newNoteId = maxId + 1
+    note.id = newNoteId
+    notes.push(note)
+    note.dateCreated = new Date().toLocaleString();
+}
+
+const newerNote = {
+    subject: "Functions",
+    date: "May 28, 2021",
+    feeling: "It was hard to understand at first, but I think I am getting it.",
+    timeSpent: 10
 };
+
+
+createNote(newerNote);
+
+    for (const note of notes) {
+    console.log(`Note ${note.id}
+    I was learning ${note.subject}
+    The minutes I spent studying was ${note.timeSpent}
+    What I felt during this was: ${note.feeling}.`);
+};
+
+
+console.log(notes);
